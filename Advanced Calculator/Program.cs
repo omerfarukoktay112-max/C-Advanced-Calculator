172e225c30cf37002af000d545c12d4e6743dbcb
omerfarukoktay112-max <omerfarukoktay112@gmail.com>
2025-11-12 19:15:42 +0300

    feat: Initial commit of C# Console Calculator

---
+++ Advanced Calculator/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Threading;
+
+namespace Advanced_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advanced Calculator");
+            Console.WriteLine("--------------------");
+
+            while (true)
+            {
+                Console.WriteLine("Action To Be Taken");
+                Console.WriteLine("--------------------");
+                Console.WriteLine("Addition -> +");
+                Console.WriteLine("Subtraction -> -");
+                Console.WriteLine("Multiplication -> *");
+                Console.WriteLine("Division -> /");
+                Console.WriteLine("Square Root -> sr");
+                Console.WriteLine("Exponentiation -> ex");
+                Console.WriteLine("Logarithm -> lg");
+                Console.WriteLine("Absolute Value -> mv");
+                Console.WriteLine("--------------------");
+                Console.Write("Action to be taken: ");
+                string girdi = Console.ReadLine();
+                Console.WriteLine();
+                Console.WriteLine("--------------------");
+
+                double transaction_result = 0;
+
+                if (girdi == "+" || girdi == "-" || girdi == "/" || girdi == "*" ||
+                    girdi == "sr" || girdi == "ex" || girdi == "lg" || girdi == "mv")
+                {
+                    if (girdi == "+" || girdi == "-" || girdi == "/" || girdi == "*" || girdi == "ex")
+                    {
+                        if (girdi == "+" || girdi == "-" || girdi == "/" || girdi == "*")
+                        {
+                            Console.Write("1st number: ");
+                            double number1 = Convert.ToDouble(Console.ReadLine());
+                            Console.Write("2nd number: ");
+                            double number2 = Convert.ToDouble(Console.ReadLine());
+
+                            switch (girdi)
+                            {
+                                case "+":
+                                    transaction_result = number1 + number2;
+                                    Console.WriteLine("Addition result: " + transaction_result);
+                                    break;
+
+                                case "-":
+                                    transaction_result = number1 - number2;
+                                    Console.WriteLine("Subtraction result: " + transaction_result);
+                                    break;
+
+                                case "*":
+                                    transaction_result = number1 * number2;
+                                    Console.WriteLine("Multiplication result: " + transaction_result);
+                                    break;
+
+                                case "/":
+                                    if (number2 == 0)
+                                        Console.WriteLine("Error: Division by zero is not allowed!");
+                                    else
+                                    {
+                                        transaction_result = number1 / number2;
+                                        Console.WriteLine("Division result: " + transaction_result);
+                                    }
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            Console.Write("Base number: ");
+                            double number1 = Convert.ToDouble(Console.ReadLine());
+                            Console.Write("Exponent value: ");
+                            double number2 = Convert.ToDouble(Console.ReadLine());
+                            transaction_result = Math.Pow(number1, number2);
+                            Console.WriteLine("Exponentiation result: " + transaction_result);
+                        }
+                    }
+                    else
+                    {
+                        Console.Write("Enter the number: ");
+                        double number1 = Convert.ToDouble(Console.ReadLine());
+
+                        switch (girdi)
+                        {
+                            case "lg":
+                                transaction_result = Math.Log10(number1);
+                                Console.WriteLine("Logarithm result: " + transaction_result);
+                                break;
+
+                            case "sr":
+                                transaction_result = Math.Sqrt(number1);
+                                Console.WriteLine("Square root result: " + transaction_result);
+                                break;
+
+                            case "mv":
+                                transaction_result = Math.Abs(number1);
+                                Console.WriteLine("Absolute value: " + transaction_result);
+                                break;
+                        }
+                    }
+
+                    Console.WriteLine("--------------------");
+                    Console.Write("Will you continue? (yes - no): ");
+                    string vote = Console.ReadLine().ToLower();
+
+                    if (vote == "yes")
+                    {
+                        Console.WriteLine("Returning to main menu...");
+                        Thread.Sleep(600);
+                        Console.Clear();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Exiting...");
+                        Thread.Sleep(1000);
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid operation, restarting...");
+                    Thread.Sleep(1800);
+                    Console.Clear();
+                }
+            }
+        }
+    }
+}
---
+++ Advanced Calculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Advanced Calculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Advanced Calculator")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("b2ba7ebe-09ac-4dec-b10e-a68b0eca02b2")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
